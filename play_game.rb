8e62881747fd11147a48a49b0b3d028004640dac
Conner Hiatt <connerh@me.com>
2019-02-16 14:48:00 +0000

    catch any errors

--- play_game.rb
+++ play_game.rb
@@ -2,4 +2,8 @@ require_relative 'V1/characters'
 require_relative 'V1/helpers'
 require_relative 'V1/game_engine'
 
-V1::GameEngine.play_game
+begin
+  V1::GameEngine.play_game
+rescue => e
+  puts e
+end
